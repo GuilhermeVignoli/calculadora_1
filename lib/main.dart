03cbef365f6920193952a6026dd358abad3cc239
Guilherme Vignoli Bras <guilherme.vignoli@outlook.com>
2021-03-28 23:02:51 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:calculadora/pages/calculadora_page.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 14,
+      navigateAfterSeconds: new MaterialApp(
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: CalculatorPage(),
+    ),
+      title: new Text('Welcome In SplashScreen',
+      style: new TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20.0
+      ),),
+      image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+      backgroundColor: Colors.white,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 100.0,
+      onClick: ()=>print("Flutter Egypt"),
+      loaderColor: Colors.red
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
